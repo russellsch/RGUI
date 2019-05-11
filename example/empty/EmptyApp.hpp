4caf117096bb6b9efd82fa2f00e3e5ef5e423274
russellsch <34686+russellsch@users.noreply.github.com>
2019-05-11 15:30:35 -0400

    Adding override keywords to headers

--- example/empty/EmptyApp.hpp
+++ example/empty/EmptyApp.hpp
@@ -6,16 +6,16 @@ class EmptyApp : public RGApp{
     RGRoot* root;
 
 	public:
-		void setup();
-		void draw();
+		void setup() override;
+		void draw() override;
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
+		void keyPressed (int key) override;
+		void keyReleased(int key) override;
+		void mouseMoved(int x, int y ) override;
+		void mouseDragged(int x, int y, int button) override;
+		void mousePressed(int x, int y, int button) override;
+		void mouseReleased(int x, int y, int button) override;
+		void windowResized(int w, int h) override;
 
 };
 
--- include/rgui/something.h
+++
@@ -1,7 +0,0 @@
-#ifndef RGUI_SOMETHING
-#define RGUI_SOMETHING
-
-void some_function(int a);
-
-
-#endif //RGUI_SOMETHING
